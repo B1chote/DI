79187c1e2e498e336afe04c88e42c011c20982f0
B1chote <157001306+B1chote@users.noreply.github.com>
2024-10-16 08:58:10 +0200

    Add files via upload

---
+++ src/main/java/com/example/di_ud1_tarea1_victorperezgonzalez/LoginApplication.java
@@ -0,0 +1,24 @@
+package com.example.di_ud1_tarea1_victorperezgonzalez;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class LoginApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("LoginView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 616, 396);
+        stage.setTitle("Log In");
+        stage.setScene(scene);
+        stage.setMinHeight(350);
+        stage.setMinWidth(500);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/di_ud1_tarea1_victorperezgonzalez/LoginController.java
@@ -0,0 +1,24 @@
+package com.example.di_ud1_tarea1_victorperezgonzalez;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML
+    private Label warningText;
+    @FXML
+    private TextField user;
+    @FXML
+    private PasswordField passwrd;
+
+    @FXML
+    protected void signinButtonClick() {
+        if (user.getText().isEmpty() || passwrd.getText().isEmpty()){
+            warningText.setText("complete all fields");
+        }
+        else {
+            warningText.setText("user unknown");}
+    }
+}
